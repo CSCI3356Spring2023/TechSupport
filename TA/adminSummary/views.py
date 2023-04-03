34af3ffa859a220a1e8e6f53cf50063937e43887
andrewyang0213 <andrewseanyang0213@gmail.com>
2023-04-02 20:49:38 -0400

    added course count display

--- TA/adminSummary/views.py
+++ TA/adminSummary/views.py
@@ -3,5 +3,6 @@ from InstructorAddCourse.models import InstructorAddCourse
 
 def admin_summary_view(response):
     course_objects = InstructorAddCourse.objects.all()
-    context = {'course_objects': course_objects}
+    course_count = course_objects.count()
+    context = {'course_objects': course_objects, 'course_count': course_count}
     return render(response, "adminSummary.html", context)
