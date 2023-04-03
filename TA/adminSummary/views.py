0811c11919fdfdc6dcddab9a0c54995047f5d96f
andrewyang0213 <andrewseanyang0213@gmail.com>
2023-04-02 20:03:22 -0400

    added connectivity to course model objects

--- TA/adminSummary/views.py
+++ TA/adminSummary/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from InstructorAddCourse.models import InstructorAddCourse
 
 def admin_summary_view(response):
-    return render(response, "adminSummary.html", {})
+    course_objects = InstructorAddCourse.objects.all()
+    context = {'course_objects': course_objects}
+    return render(response, "adminSummary.html", context)
