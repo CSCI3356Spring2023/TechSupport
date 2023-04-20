693898f0cd3e5827a65715244e5120ab2e425e58
andrewyang0213 <andrewseanyang0213@gmail.com>
2023-04-20 17:33:30 -0400

    added filter connection to dept codes

--- TA/adminSummary/views.py
+++ TA/adminSummary/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from InstructorAddCourse.models import InstructorAddCourse
 
+dept_keys = ['CSCI', 'ECON', 'PHIL']
+status_keys = ['Open', 'Closed']
 
 def admin_summary_view(response):
     search_query = response.GET.get('q', '')
     status_filter = response.GET.get('status', '')
+    dept_filter = response.GET.get('dept-code', '')
 
     course_objects = InstructorAddCourse.objects.all()
     applied_filters = []
@@ -14,8 +17,14 @@ def admin_summary_view(response):
             course_name__icontains=search_query)
         applied_filters.append(('q', search_query))
 
+    if dept_filter:
+        if dept_filter in dept_keys:
+            course_objects = [course for course in course_objects if get_dept(
+                course) == dept_filter]
+            applied_filters.append(('dept-code', dept_filter))
+
     if status_filter:
-        if status_filter in ['Open', 'Closed']:
+        if status_filter in status_keys:
             course_objects = [course for course in course_objects if get_status(
                 course) == status_filter]
             applied_filters.append(('status', status_filter))
@@ -30,6 +39,13 @@ def admin_summary_view(response):
 
     return render(response, "adminSummary.html", context)
 
+
+def get_dept(course):
+    dept_code = ""
+    for i in course.course_number:
+        if i.isalpha():
+            dept_code = "".join([dept_code, i])
+    return dept_code
 
 def get_status(course):
     return "Open" if course.curr_num_ta < course.num_ta_needed else "Closed"
