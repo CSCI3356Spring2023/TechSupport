dcb8698b85c2f33555a97de6edfaedc8c0946784
davidxosei <davidxosei@gmail.com>
2023-04-20 23:28:37 -0400

    Updated Registration page
    Eagle ID, Year, and Major are now required when signing up as a student

--- TA/login/forms.py
+++ TA/login/forms.py
@@ -1,34 +1,82 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django import forms
 from .models import CustomUser
 
-# Define choices for the role field
-ROLE_CHOICES = [    ('S', 'Student'),    ('T', 'Teacher'),    ('A', 'Admin'),]
 
-from django.contrib.auth.forms import AuthenticationForm
-from django import forms
+# Define choices for the role field
+ROLE_CHOICES = [   
+   ('S', 'Student'),   
+   ('T', 'Teacher'),   
+   ('A', 'Admin'),
+]
+
+
+# Define choices for the major field
+MAJOR_CHOICES = [
+   ('NA', 'NA'),
+   ('BA', 'Bachelor of Arts in Computer Science'),
+   ('BS', 'Bachelor of Science in Computer Science'),
+   ('Minor', 'Computer Science Minor'),
+]
+
+
+# Define choices for the cohort field
+YEAR_CHOICES = [
+   ('NA', 'NA'),
+   ('Freshman', 'Freshman'),
+   ('Sophomore', 'Sophomore'),
+   ('Junior', 'Junior'),
+   ('Senior', 'Senior'),
+]
+
 
 class CustomAuthenticationForm(AuthenticationForm):
-    role = forms.CharField()
+   role = forms.CharField()
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Username'})
-        self.fields['password'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Password'})
-        #self.fields['role'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Role'})
 
-    def clean(self):
-        cleaned_data = super().clean()
-        role = cleaned_data.get('role')
-        if not role:
-            raise forms.ValidationError('Role is required.')
-        return cleaned_data
+   def __init__(self, *args, **kwargs):
+       super().__init__(*args, **kwargs)
+       self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Username'})
+       self.fields['password'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Password'})
+       #self.fields['role'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Role'})
+
+
+   def clean(self):
+       cleaned_data = super().clean()
+       role = cleaned_data.get('role')
+       if not role:
+           raise forms.ValidationError('Role is required.')
+       return cleaned_data
+
+
 
 
 # Create a custom registration form that includes the role field
 class RegistrationForm(UserCreationForm):
-    role = forms.ChoiceField(choices=ROLE_CHOICES)
+   role = forms.ChoiceField(choices=ROLE_CHOICES)
 
-    class Meta(UserCreationForm.Meta):
-        model = CustomUser
-        fields = ['username', 'password1', 'password2', 'role']
+
+   major = forms.ChoiceField(choices=MAJOR_CHOICES, required=False)
+   eagle_id = forms.CharField(max_length=8, required=False)
+   year = forms.ChoiceField(choices=YEAR_CHOICES, required=False)
+
+
+   def clean(self):
+       cleaned_data = super().clean()
+       role = cleaned_data.get('role')
+       major = cleaned_data.get('major')
+       eagle_id = cleaned_data.get('eagle_id')
+       year = cleaned_data.get('year')
+
+
+       if role == 'S':
+           if (major == 'NA' or not eagle_id or year == 'NA'):
+               raise forms.ValidationError("Please fill out all required fields for student registration")
+
+
+       return cleaned_data
+
+
+   class Meta(UserCreationForm.Meta):
+       model = CustomUser
+       fields = ['username', 'password1', 'password2', 'role', 'major', 'eagle_id', 'year']
--- TA/login/models.py
+++ TA/login/models.py
@@ -1,25 +1,37 @@
 from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import AbstractUser
 
-class CustomUser(AbstractUser):
-    ROLE_CHOICES = (
-        ('S', 'Student'),
-        ('T', 'Teacher'),
-        ('A', 'Admin'),
-    )
-    role = models.CharField(max_length=1, choices=ROLE_CHOICES)
-    
-    applications = models.PositiveIntegerField(default=0)
 
-    def can_apply(self):
-        if self.role == 'S' and self.applications >= 5:
-            return False
-        else:
-            return True
-    
-    def increment_applications(self):
-        if self.role == 'S':
-            self.applications += 1
-            self.save()
+# Create your models here.
+
+
+class CustomUser(AbstractUser):
+   ROLE_CHOICES = (
+       ('S', 'Student'),
+       ('T', 'Teacher'),
+       ('A', 'Admin'),
+   )
+   role = models.CharField(max_length=1, choices=ROLE_CHOICES)
+
+
+   applications = models.PositiveIntegerField(default=0)
+
+
+   major = models.CharField(max_length=100, blank=True)
+   eagle_id = models.CharField(max_length=8, blank=True)
+   year = models.CharField(max_length=20, blank=True)
+
+
+   def can_apply(self):
+       if self.role == 'S' and self.applications >= 5:
+           return False
+       else:
+           return True
+
+
+   def increment_applications(self):
+       if self.role == 'S':
+           self.applications += 1
+           self.save()
+
+
