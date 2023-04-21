9d2e86f43ab95062c0fe3d625dbfbd7409ed4bc9
Justin Cheung <jc030201@gmail.com>
2023-04-21 05:17:45 -0400

    modify instructorsummary views.py

--- TA/instructorSummary/views.py
+++ TA/instructorSummary/views.py
@@ -4,8 +4,20 @@ from application.models import Application
 
 # Create your views here.
 def instructor_summary_view(response):
+    # database objects
     course_objects = InstructorAddCourse.objects.all()
     application_objects = Application.objects.all()
+
+    # search terms
+    search_query = response.GET.get('q', '')
+
+    applied_filters = []
+
+    if search_query:
+        course_objects = course_objects.filter(
+            course_name__icontains=search_query)
+        applied_filters.append(('q', search_query))
+
     context = {'course_objects': course_objects,
                'application_objects': application_objects}
     return render(response, "instructorSummary.html", context)
